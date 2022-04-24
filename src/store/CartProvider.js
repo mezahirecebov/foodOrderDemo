729e3edaac4268978cc3f703ad15dccea55aae5f
Mezahir  Rajabov <mezahir.rajabov.wesii@gmail.com>
2022-04-24 02:41:48 +0200

    add firebase requests

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -53,6 +53,11 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmout,
     };
   }
+
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -74,11 +79,17 @@ const CartProvider = (props) => {
       id: id,
     });
   };
+
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
   return (
     <CartContext.Provider value={cartContext}>
